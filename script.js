d85d53f6c1ee2622a1421e726202a75bceddadb4
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2025-12-17 06:39:18 +0000

    Added error msg to display when city is invalid

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appID=${apiKey}`);
-    var data = await response.json();
+
+    if(response.status === 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+        var data = await response.json();
 
     console.log(data);
 
@@ -28,11 +33,14 @@ async function checkWeather(city) {
         weatherIcon.src = "images/partly-cloudy.png"
     }else if(data.weather[0].main === "Snow"){
         weatherIcon.src = "images/snow.png"
-    }else{
+    }else if(data.weather[0].main === "Rain"){
         weatherIcon.src = "images/rain.png"
     }
 
     document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+    }
+    
 }
 
 searchBtn.addEventListener("click", ()=>{
