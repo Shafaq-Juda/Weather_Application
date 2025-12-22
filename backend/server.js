b3669e3715a024e6ad9a83c785b98a120e7dcaab
Shafaq Juda <148673194+Shafaq-Juda@users.noreply.github.com>
2025-12-22 06:32:32 +0000

    Edit the port to make it run on render

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    //console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on posrt ${PORT}`);
 });
